7bd0b8e18cc0920e1064617d67445045e091a207
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-20 10:28:27 -0700

    commit 1 just to get the github working

---
+++ quiz.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My jquery quiz</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+	<div class="dan">
+		<p> 
+		This is Dan's first name.
+		</p>
+	</div>
+	<div class="iwao">
+		<p>
+		This is dan's last name.
+		</p>
+	</div>
+
+</body>
+
+</html>
